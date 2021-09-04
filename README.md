139fa71a677e6e247a103c7f29e1f7cd1eb290e4
AleTornesello <alessandro.tornesello99@gmail.com>
2021-09-04 16:19:02 +0200

    feat: initial project

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# CTFVault-client
-Frontend of CTFVault - a simple search-oriented CTF writeups container
+# ctfvault-client
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <div id="header-container">
+      <div>Logo</div>
+      <div>powered by</div>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#header-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  height: 120px;
+  background-color: #444444;
+  color: #ffffff;
+}
+</style>
